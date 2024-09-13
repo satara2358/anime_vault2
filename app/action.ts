f2e21627fcfd61bddeb24621cdf5284421d9f94a
Satara2358 <25549072+satara2358@users.noreply.github.com>
2024-09-13 18:44:54 -0500

    Update API endpoint and limit in fetchAnime function

--- app/action.ts
+++ app/action.ts
@@ -3,11 +3,11 @@
 
 export const fetchAnime = async (page: number) => {
   //const response = await  fetch("https://shikimori.one/api/animes");
-  const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=12&order=popularity`);
+  const response = await fetch(`https://shikimori.one/api/animes?page=${page}&limit=8&order=popularity`);
 
   const data = await response.json();
 
-  console.log(data)
+  // console.log(data)
 
   return data;
 }
